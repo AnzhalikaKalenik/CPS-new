d59252b3f918186f43ef12b022c4f560903267a2
Anzhelika Kalenik <anzhalikaremez@mail.ru>
2024-07-05 15:05:20 +0300

    modified the navbar

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,14 @@
 document.querySelector('.burger').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('open');
+
+    if (document.querySelector('.nav').classList.contains('open')){
+        document.querySelector('.body').style.overflow='hidden';
+    }
+    else{
+        document.querySelector('.body').style.overflow='visible';
+    }
+
 });
 
 ///читать далее
